a245724e55bd1267bd7044483b60d89adabb9907
Ziliang Peng <ziliangdotme@gmail.com>
2015-01-28 21:05:42 -0800

    docker run not silent

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ echo 'run'
 git clone https://github.com/ziliangdotme/dind.git > /dev/null
 cd dind
 docker build -t dind . > /dev/null
-docker run --privileged -it dind > /dev/null
+docker run --privileged -it dind
 
